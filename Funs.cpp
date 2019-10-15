12e5b72f4f64958e9d993f23eaef26e1dc72fe2d
迟谋 <705359976@qq.com>
2019-10-15 08:55:14 +0800

    基础测试

--- Funs.cpp
+++ Funs.cpp
@@ -6,8 +6,10 @@
 */
 #include "pch.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
-void UnitTransform() {
+const int TI = 1;
+void UnitTransform() {	
 	cout << "这是一个单位转换工具——克转盎司\n请选择克或盎司：1\\2\n";
 	int selectUnit;
 	cin >> selectUnit;
@@ -29,4 +31,16 @@ void UnitTransform() {
 		tempOut = tempIn * 28.349523;
 		cout << tempIn << "盎司=" << tempOut << "克\n";
 	}
+}
+void IOSum() {
+	using namespace std;
+	ifstream inStream;
+	ofstream outStream;
+	inStream.open("infile.dat");//需要创建，在vcxproj同级目录下
+	outStream.open("outfile.dat");//可以自动生成
+	int a, b, c;
+	inStream >> a >> b >> c;//输入文件内的数据用空格或回车分开
+	outStream << (a + b + c) << endl;
+	inStream.close();//注意用完要关
+	outStream.close();
 }
---
+++ MainTest.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <string>
+#define NDEBUG
+#include <cassert>
+
+using namespace std;
+void IOSum();
+int main(){		
+	IOSum();
+}
